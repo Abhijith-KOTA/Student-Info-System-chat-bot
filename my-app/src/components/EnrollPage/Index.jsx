abf492cec5230811b059715335733db151623f1d
Kota Abhijith <128505664+Abhijith-KOTA@users.noreply.github.com>
2023-08-12 17:07:28 +0000

    Revert "dep"
    This reverts commit 308241db8599b9a041289c9df473619c3ae041d5.

--- my-app/src/components/EnrollPage/Index.jsx
+++ my-app/src/components/EnrollPage/Index.jsx
@@ -5,7 +5,7 @@ const EnrollPage=()=>{
     return(
         <div className='enroll-bg-container' >
             <h1>Enter into Student Info System</h1>
-            <Link to="https://abhijith-kota.github.io/Student-Info-System-chat-bot/details" >
+            <Link to="/details" >
                 <button className="enroll-btn" type='button'>Enroll !</button>
             </Link>
         </div>
